dce8082fd966a447c5e2442de5a635ad88d78b06
Alex <alexiseliudrdz@gmail.com>
2016-07-19 16:44:14 -0500

    migrations

--- database/migrations/2016_07_19_212725_create_descriptions_table.php
+++ database/migrations/2016_07_19_212725_create_descriptions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateDescriptionsTable extends Migration
 {
     /**
      * Run the migrations.
