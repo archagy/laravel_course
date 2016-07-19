76ea0944c2592a3e8a63bc105e52d63fe3539773
Alex <alexiseliudrdz@gmail.com>
2016-07-19 17:55:48 -0500

    route with prefix group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('products', function(){
-	return App\Product::all();
+
+Route::group(['prefix' => 'api'], function() {
+	Route::get('products', ['as'=>'products', function(){
+		return App\Product::all();
+	}]);
 });
