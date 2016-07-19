eedb6bce63b07031243c820afe5ddc8b9c44f3fd
Alex <alexiseliudrdz@gmail.com>
2016-07-19 16:51:34 -0500

    Fix syntax error

--- database/migrations/2016_07_19_212725_create_descriptions_table.php
+++ database/migrations/2016_07_19_212725_create_descriptions_table.php
@@ -15,7 +15,7 @@ class CreateDescriptionsTable extends Migration
         Schema::create('descriptions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();
-            $table->foreign('product_id')->references->on('products');
+            $table->foreign('product_id')->references('id')->on('products');
             $table->text('body');
             $table->timestamps();
         });
